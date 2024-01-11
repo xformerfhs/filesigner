c0d1d265ea4455d29dfce4755ac1a226a8b22413
Frank Schwab <frank.schwab@live.de>
2024-01-11 19:27:16 +0100

    chore: Better comment for maphelper.SortedKeys

--- maphelper/map_helper.go
+++ maphelper/map_helper.go
@@ -6,7 +6,7 @@ import (
 	"slices"
 )
 
-// SortedKeys returns a slice with the keys of a map ordered by value
+// SortedKeys returns the keys of the map m. The keys will be sorted.
 func SortedKeys[K cmp.Ordered, V any](m map[K]V) []K {
 	result := maps.Keys(m)
 	slices.Sort(result)
