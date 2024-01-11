428cfa6032fd3817c1a8dff76ee87099675ab7fd
Frank Schwab <frank.schwab@live.de>
2024-01-11 19:30:35 +0100

    chore: Added warning to comment of stringhelper.StringBytes.

--- stringhelper/string_helper.go
+++ stringhelper/string_helper.go
@@ -3,7 +3,7 @@ package stringhelper
 import "unsafe"
 
 // StringBytes returns a byte slice that points to the bytes of the supplied string.
-// No bytes are copied.
+// No bytes are copied. Attention: This is *unsafe*! Do not change those bytes!
 func StringBytes(s string) []byte {
 	// This is a streamlined version of
 	// https://josestg.medium.com/140x-faster-string-to-byte-and-byte-to-string-conversions-with-zero-allocation-in-go-200b4d7105fc .
